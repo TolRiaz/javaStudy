d4bdc6f92879f654e8ed8f15f74e9baf02e2adca
root <tolriaz@gmail.com>
2023-12-04 08:46:48 +0000

    맨 끝에 /가 없는 경우 넣도록 하는 코드 추가

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,35 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.demo.util.SlashInterceptor;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
+
+	private final SlashInterceptor slashInterceptor;
+
+	// slash interceptor 선언
+	public DemoApplication(SlashInterceptor slashInterceptor) {
+		this.slashInterceptor = slashInterceptor;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	// add interceptors for slash
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(slashInterceptor);
+	}
+
+	@Override
+	public void addViewControllers( ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/order");
 	}
 
 }
